e25981e3902c0fd9f98a136cb3737f87c335e516
kushal-mindgrep <kushal@mindgrep.com>
2022-10-20 16:08:37 +0530

    feat: add server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+require('dotenv').config();
+const Port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Successfull',
+  });
+});
+
+app.listen(Port, (req, res) => {
+  console.log(`http://localhost:${Port}`);
+});
+Footer;
